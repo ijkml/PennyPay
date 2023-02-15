451a24cf7a40f209f0f75b262a2c6e435ed32522
ML <me.mlaure@gmail.com>
2023-02-15 20:44:52 +0100

    create CTA section

---
+++ components/Home/CTA.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <section id="get-started" role="region" aria-labelledby="heading_get-started">
+    <div class="cta-wrapper">
+      <div class="cta-container">
+        <div class="pre-heading dark">Let's get started</div>
+        <h2 id="heading_get-started" class="heading dark">
+          Create an account, start accepting secure&nbsp;payments
+        </h2>
+        <div class="actions">
+          <VButton to="/">Create an Account</VButton>
+          <NuxtLink class="cta-2nd" to="/">Pricing Options</NuxtLink>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss">
+#get-started {
+  @apply sm:(px-4 py-16) md:(px-8 py-20);
+}
+
+.cta-wrapper {
+  background: url('@img/bg-spot.svg') center / cover no-repeat;
+
+  @apply py-16 px-4 space-y-4 max-w-60rem mx-auto
+    sm:(rounded-3xl) md:(px-16 rounded-2.5rem);
+}
+
+.cta-container {
+  @apply max-w-55ch mx-auto text-center grid;
+}
+
+.heading {
+  @apply font-semibold my-4;
+}
+
+.actions {
+  @apply mt-3 inline-grid mx-auto gap-4;
+}
+
+.cta-2nd {
+  @apply underline underline-(offset-16 transparent dashed)
+    outline-none op-55 transition-all-250;
+
+  &:where(:hover, :focus-visible) {
+    @apply op-100 underline-(offset-8 zinc-5/45);
+  }
+}
+</style>
