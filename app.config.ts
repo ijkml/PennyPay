74369b3a296b5ecb56b8cc03bd15c758f25a3b45
ML <me.mlaure@gmail.com>
2023-02-22 18:20:06 +0100

    revert exps

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,7 @@
 export default defineAppConfig({
   umami: {
     host: 'https://ml-umami.netlify.app/',
-    // id: 'dead95c9-f13c-4728-a825-ac1eb173ed97',
-    id: '',
+    id: 'dead95c9-f13c-4728-a825-ac1eb173ed97',
     autoTrack: true,
   },
 });
