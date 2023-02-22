b7d428caf39717461864ebcb079cd82a89a68237
ML <me.mlaure@gmail.com>
2023-02-22 18:17:48 +0100

    exp: test prod error

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,8 @@
 export default defineAppConfig({
   umami: {
-    // host: 'https://ml-umami.netlify.app/',
-    id: 'dead95c9-f13c-4728-a825-ac1eb173ed97',
-    host: 'https://mx-umami.netlify.app/',
+    host: 'https://ml-umami.netlify.app/',
+    // id: 'dead95c9-f13c-4728-a825-ac1eb173ed97',
+    id: '',
     autoTrack: true,
   },
 });
